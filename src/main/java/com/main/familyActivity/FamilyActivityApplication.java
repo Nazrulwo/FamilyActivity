bc77a7fa173012eea37decbc320639ed5bdce8d5
Nazrul Islam <nazrulwo@gmail.com>
2024-09-03 16:17:23 +0200

    Initial commit

---
+++ src/main/java/com/main/familyActivity/FamilyActivityApplication.java
@@ -0,0 +1,13 @@
+package com.main.familyActivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyActivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/familyActivity/FamilyActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.familyActivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
